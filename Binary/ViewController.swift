2245872912b2528616d013da8f8be2f3e47306ba
William Bodily <william.bodily@gmail.com>
2018-07-30 22:43:48 -0400

    Initial commit

--- Binary/ViewController.swift
+++ Binary/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let placeholder = NSAttributedString(string: "Enter a value...", attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), NSAttributedStringKey.font: UIFont(name: "Menlo", size: 36.0)!])
 
+    @IBOutlet weak var valueEntryTextField: UITextField!
+    @IBOutlet weak var binaryButton: UIButton!
+    @IBOutlet weak var decimalButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        valueEntryTextField.attributedPlaceholder = placeholder
+        valueEntryTextField.addTarget(self, action: #selector(textFieldTextDidChange), for: .editingChanged)
+        toggleButtons(buttonState: false, buttonOpacity: 0.5)
+    }
+    
+    @objc func textFieldTextDidChange() {
+        if valueEntryTextField.text == "" {
+            toggleButtons(buttonState: false, buttonOpacity: 0.5)
+        } else {
+            toggleButtons(buttonState: true, buttonOpacity: 1.0)
+        }
+    }
+    
+    func toggleButtons(buttonState: Bool, buttonOpacity: CGFloat) {
+        binaryButton.isEnabled = buttonState
+        binaryButton.alpha = buttonOpacity
+        decimalButton.isEnabled = buttonState
+        decimalButton.alpha = buttonOpacity
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func binaryButtonWasPressed(_ sender: Any) {
     }
-
-
+    
+    @IBAction func decimalButtonWasPressed(_ sender: Any) {
+    }
+    
 }
 
